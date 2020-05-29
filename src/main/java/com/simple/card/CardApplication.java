a7dce081120272f9962ba629f60da3d7ebfd8b6d
小怪 <admin@ioy.cc>
2020-05-29 17:42:32 +0800

    Initial commit

---
+++ src/main/java/com/simple/card/CardApplication.java
@@ -0,0 +1,18 @@
+package com.simple.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class CardApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CardApplication.class);
+    }
+}
